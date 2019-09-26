e1ef16ce95de133e3c15168c391cea3e73c5bf58
vanshkhanna17 <51960279+vanshkhanna17@users.noreply.github.com>
2019-09-26 12:33:58 +0530

    Update script.js

--- Assessment 1/script/script.js
+++ Assessment 1/script/script.js
@@ -26,7 +26,7 @@ var addrow = function(value,price){
 }
 $(document).on("click", "button.rmv", function() {
     indx= $(this).data("indx");
-    $("#mdbdy1 > tr#"+indx).remove("");
+    $("#mdbdy1 > div > tr#"+indx).remove("");
 });
 $("#sub").on("click", function(){
     
@@ -38,5 +38,6 @@ $("#sub").on("click", function(){
     console.log(addr);
     var txt= "<p>Thank you "+nam+" - "+ema+".<br>"+"Your order will be delivered to "+addr+" as soon as possible.</p>"
     $("#mdbdy3").append(txt);
-    $("#mdbdy1 > tr").remove();
+    $("#mdbdy1 > div > tr").remove();
+    $("#cst").html("");
 });
