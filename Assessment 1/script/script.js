6950148dd5756b6abc2ca6454772b40fe2d4e0e7
vanshkhanna17 <51960279+vanshkhanna17@users.noreply.github.com>
2019-09-26 12:36:40 +0530

    Update script.js

--- Assessment 1/script/script.js
+++ Assessment 1/script/script.js
@@ -6,27 +6,32 @@ $(document).on("click", "button.btn1", function(){
  value=$(this).val();
  pr= value.toString().split("Rupees")[1];
  pr=parseInt(pr);
- price=price+pr
+ price=price+pr;
  $("#cst").html("<strong>Total Amount: Rupees "+price+"</strong>");
  console.log(price);
-addrow(value,price);
+addrow(value,pr);
 id++;
 });
-var addrow = function(value,price){
+var addrow = function(value,pr){
     var tr = "";
     tr += "<tr id=\""+id+"\">";
     tr += "<td>";
     tr += value;
     tr += "</td>";
     tr += "<td>";
-    tr+= "<button type=\"button\" class=\"rmv\" data-indx=\""+id+"\">Remove</button>";
+    tr+= "<button type=\"button\" class=\"rmv\" data-indx=\""+id+"\" data-value=\""+pr+"\">Remove</button>";
     tr+="</td>";
     tr += "</tr>";
     $("#mdbdy1 > div").append(tr);
 }
 $(document).on("click", "button.rmv", function() {
     indx= $(this).data("indx");
+    var prc= $(this).data("value");
+    console.log(prc)
+    price=price-prc;
+    $("#cst").html("<strong>Total Amount: Rupees "+price+"</strong>");
     $("#mdbdy1 > div > tr#"+indx).remove("");
+
 });
 $("#sub").on("click", function(){
     
